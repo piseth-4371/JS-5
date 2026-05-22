8a4bf96c953f45d5323b7cbd337fc79499b873e1
piseth-4371 <pisethkhat4371@gmail.com>
2026-05-22 23:04:47 +0700

    last update

--- Toast Notification/script.js
+++ Toast Notification/script.js
@@ -1,13 +1,20 @@
 let toastBox = document.querySelector(".toastBox");
 let SuccessMassage = '<i class="fa-solid fa-circle-check mx-5 text-4xl text-green-700"></i>Successfully  Sumbited';
 let ErrorMassage = '<i class="fa-solid fa-circle-xmark mx-5 text-4xl text-red-700"></i>Please fix the error!';
-let InvalidMassage = '<i class="fa-solid fa-circle-exclamation mx-5 text-4xl text-yellow-700 "></i>Invalid input, check again!';
+let InvalidMassage = '<i class="fa-solid fa-circle-exclamation mx-5 text-4xl text-yellow-700"></i>Invalid input, check again!';
 
-function showToast(Massage){
+function showToast(massage){
     let toast = document.createElement("div");
     toast.classList.add("toast");
-    toast.innerHTML = Massage;
+    toast.innerHTML = massage;
     toastBox.appendChild(toast);
+
+    if(massage.includes('error')){
+        toast.classList.add('after:bg-red-800');
+    }
+    if(massage.includes('Invalid')){
+        toast.classList.add('after:bg-yellow-800');
+    }
 
     setTimeout(() => {
         toast.remove();
