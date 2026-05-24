0d25d30b162419d50c6165010c421860ddb17eea
piseth-4371 <pisethkhat4371@gmail.com>
2026-05-24 12:44:56 +0700

    fix icon

--- Music Player/script.js
+++ Music Player/script.js
@@ -6,22 +6,20 @@ song.onloadedmetadata = function(){
     progress.max = song.duration;
     progress.value = song.currentTime;
 }
+
+
 function playPause(){
-    if(ctrlIcon.classList.contains("fa-pause")){
+    if(ctrlIcon.classList.contains("fi-sr-pause")){
         song.pause();
-        ctrlIcon.classList.remove("fa-pause");
-        ctrlIcon.classList.add("fa-play");
+        ctrlIcon.classList.replace("fi-sr-pause", "fi-sr-play");
     }else{
         song.play();
-        ctrlIcon.classList.add("fa-pause");
-        ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.replace("fi-sr-play", "fi-sr-pause");
+        
+        setInterval(() =>{
+            progress.value =song.currentTime
+        }, 600);
     }
-}
-
-if(song.play()){
-    setInterval(() =>{
-        progress.value =song.currentTime
-    }, 500);
 }
 
 progress.onchange = function(){
